c9a5344087f1a86baa0ab841ce6f55ad65f4ae29
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-10-24 09:17:08 +0100

    fixed another bug

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git status && \
 read -p "Commit description: " desc
-git pull && \  #Always better to pull before push to resolve merge conflicts locally.
+git pull && \
 git add . && \
 git add -u && \
 git commit -m "$desc" && \
